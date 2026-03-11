c57a29c65a56e20b19fcbc3d7bb29538259d511b
Tony Chen <tonychengangyi@gmail.com>
2026-03-11 12:41:32 -0700

    Added a blog post.

--- src/pages/Reflect.tsx
+++ src/pages/Reflect.tsx
@@ -22,11 +22,22 @@ const TABS: { label: string; value: Category }[] = [
 
 const posts: Post[] = [
   {
-    slug: "lessons-from-internship",
-    date: "Jan 2026",
-    title: "What my first engineering internship actually taught me",
+    slug: "learing-zephyr-at-internship",
+    date: "Feb 2026",
+    title: "Learning Zephyr for the first time for an Internship",
     excerpt:
-      "The gap between classroom knowledge and production code is real. Here's what surprised me, what humbled me, and what I'd tell myself before starting.",
+      "Before working at Verdi, I only understood how to use STM32-based FreeRTOS oS threads to run concurrent tasks, and " +
+      "Verdi I was expected to have fluency in Zephyr to produce for the team. This task was difficult as I had no idea how to " +
+      "use the syntax, build system and general flow of this new system. Although being very challenging, I put myself up to the " +
+      "task as I wished to contribute to the company to the best of my ability as well as improve my learning on a new firmware system " +
+      "I could use in the future. This resulted in inspiring me to build my own Zephyr project during late Decemeber before I started " +
+      "my internship. I build a simple embedded network using Zephyr where I used a nRF Nordic Semiconductors board and sent BLE commands " +
+      "through their mobile nRF app to control LEDS and GPIOs on the board. This built a small base for me before I started working for Verdi. " +
+      "While working at the company, I learned to pick up syntax and ask questions whenever I could. I also utilized my tools, using AI to learn " +
+      "and give me examples of Zephyr programs. I always remembered to relate the new information I learned and compared it against things that I " +
+      "I knew before. After a month or so at the company, I became more confident in my abilites using Zephyr to create new threads, control GPIO outputs " +
+      "and utilize all the peripherals on a given PCB exactly like I would using STM32-FreeRTOS based syntax. I still have a while to go and a project to " +
+      "fully complete and will continue this blog thread in the next iteration!",
     category: "work",
   },
 ];
