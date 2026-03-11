7b3f530c1d940fcc62e1b6297ac11256c1bfd621
Tony Chen <tonychengangyi@gmail.com>
2026-03-11 05:46:48 -0700

    fix: set Vite base and BrowserRouter basename for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/website_portfolio">
       <App />
     </BrowserRouter>
   </StrictMode>,
