316a657ecec599958fdc36c4cfba950f2a1c250e
Tony Chen <tonychengangyi@gmail.com>
2026-07-06 11:22:41 -0700

    Redesigned for cloudfare pages.

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/website_portfolio">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
