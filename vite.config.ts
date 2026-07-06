f719b7d6036c8bf14da63187988b20223c293635
Tony Chen <tonychengangyi@gmail.com>
2026-07-06 12:00:16 -0700

    Adjusted vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: process.env.CF_PAGES ? '/' : '/website_portfolio/',
   plugins: [react()],
 })
